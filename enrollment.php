774644e2dd7d02648eb6f13847cde0ab130d4dca
DaniloGomez <danilogomez8@hotmail.com>
2021-03-16 17:43:09 -0500

    Enrollment full

--- enrollment.php
+++ enrollment.php
@@ -1,34 +1,35 @@
 <?php
+          // Connect database
+        $host = "localhost";
+        $dbname = "store";
+        $username = "root";
+        $password = "";
+
+        $con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+        //Build sentence SQL
+        $sql = "SELECT id, name FROM create_store";
+        //Prepare sentence SQL
+        $p = $con->prepare($sql);
+        //Execute SQL sentence
+        $result = $p->execute();
+        $store = $p -> fetchAll();
+    
+        //Build sentence SQL
+        $sql = "SELECT id, prod_name FROM products";
+        //Prepare sentence SQL
+        $p = $con->prepare($sql);
+        //Execute SQL sentence
+        $result = $p->execute();
+        $product = $p -> fetchAll();
 
 
-    // Connect database
-    $host = "localhost";
-    $dbname = "store";
-    $username = "root";
-    $password = "";
-
-    $con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
-
-    //Build sentence SQL
-    $sql = "SELECT id, name FROM create_store";
-
-    //Prepare sentence SQL
-    $p = $con->prepare($sql);
-
-    //Execute SQL sentence
-    $result = $p->execute();
-
-    $store = $p -> fetchAll();
-
-    var_dump($store);
-
-    if($result){
-        echo "Enrollment created!";
-    }
-    else{
-        echo "There is an error creating your enrollment";
-    }
-
+        if($result){
+            echo "Enrollment created!";
+        }
+        else{
+            echo "There is an error creating your enrollment";
+        }
 ?>
 
 <!DOCTYPE html>
@@ -44,22 +45,81 @@
     <center><h3 style="color:Blue;font-size:30px;">Enrollment</h3></center>
     <div style="font-size:20px;">
         <center>
-        <form action="create-store.php" method="POST">
-            Name Store <select name="store" id="">
-            <option value="">Choose</option>
+        <form action="enrollment.php" method="POST">
+
+            Name Store <select name="store_id" id="">
+            <option value=""></option>
             <?php
                 for($i=0; $i<count($store); $i++){
             ?>
-                <option value="<?php echo $store[$i]["id"] ?>"><?php echo $store[$i]["name"] ?></option>
+                <option value="<?php echo $store[$i]["id"] ?>">
+                <?php echo $store[$i]["name"] ?>
+                </option>
+
             <?php    
                 }
             ?>
             </select><br/>
-            Product <input type="text" name=products/><br/>
-            Category <input type="text" name=category><br/>
+            <br/>
+            Product <select name="product_id" id="">
+            <option value=""></option>
+            <?php
+                for($i=0; $i<count($product); $i++){
+            ?>
+                <option value="<?php echo $product[$i]["id"] ?>">
+                <?php echo $product[$i]["prod_name"] ?>
+                </option>
+            <?php
+                }
+            ?>
+            </select>
+            <br/>
+            <br/>
+            Category:
+            <input type="radio" name="category" value="0">Health
+            <input type="radio" name="category" value="1">Grosery
+            <input type="radio" name="category" value="2">Bakery
+                        
+            <br/><br/>
+
             <input type="submit" name="enrollment" value="Create Enrollment"/>
+
         </form>
         </center>
     </div>
 </body>
 </html>
+
+<?php
+
+if(!empty($_POST)){
+    $namestore = $_REQUEST["store_id"];
+    $nameproduct = $_REQUEST["product_id"];
+    $categ = $_REQUEST["category"];
+
+    // Connect database
+    $host = "localhost";
+    $dbname = "store";
+    $username = "root";
+    $password = "";
+
+    $con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    //Build sentence SQL
+    $sql = "INSERT INTO enrollment (id, store_id, product_id, category) VALUES (NULL, '$namestore', '$nameproduct', '$categ')";
+
+    //Prepare sentence SQL
+    $p = $con->prepare($sql);
+
+    //Execute SQL sentence
+    $result = $p->execute();
+
+    if($result){
+        echo "Store created succesfully";
+    }
+    else{
+        echo "There is an error";
+    }
+}
+
+?>
