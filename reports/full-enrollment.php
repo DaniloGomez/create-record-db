3a939c47f157dfca753d695fe6b091a0ff365c8c
DaniloGomez <danilogomez8@hotmail.com>
2021-03-22 10:43:15 -0500

    Table organized

---
+++ reports/full-enrollment.php
@@ -0,0 +1,101 @@
+<?php
+
+    // Connect database
+    $host = "localhost";
+    $dbname = "store";
+    $username = "root";
+    $password = "";
+
+    $con = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+
+    //Build sentence SQL
+    $sql = "SELECT cs.name, cs.location, en.category, pr.prod_name, pr.prod_price FROM `create_store` as cs JOIN enrollment as en ON cs.id = en.store_id JOIN products as pr ON en.product_id = pr.id ORDER BY cs.name ASC
+    ";
+
+    //Prepare sentence SQL
+    $p = $con->prepare($sql);
+
+    //Execute SQL sentence
+    $result = $p->execute();
+
+    $enrollments = $p -> fetchAll();    
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Enrollments List</title>
+</head>
+<body>
+
+<h1>Enrollment List Search</h1>
+    <table border="1">
+        <tr>
+            <td>
+                Name Store
+            </td>
+            <td>
+                Location Store
+            </td>
+            <td>
+                Category
+            </td>
+            <td>
+                Product
+            </td>
+            <td>
+                Price Product
+            </td>
+        </tr>
+
+<?php
+            for($i=0; $i<count($enrollments); $i++){
+?>
+                <tr>
+                    <td>
+                        <?php echo $enrollments[$i]["name"] ?>
+                    </td>
+                    <td>
+                        <?php 
+                        $location = $enrollments[$i]["location"];
+                        if($location == "0"){
+                            echo "Manizales";
+                        }
+                        else{
+                            echo "Pereira";
+                        }
+                        ?>
+                    </td>
+                    <td>
+                        <?php
+                        $category = $enrollments[$i]["category"];
+                        switch ($category){
+                            case 0:
+                                echo "Healt";
+                                break;
+                            case 1:
+                                echo "Grosery";
+                                break;
+                            case 2:
+                                echo "Bakery";
+                                break;
+                        }
+                        ?>
+                    </td>
+                    <td>
+                        <?php echo $enrollments[$i]["prod_name"] ?>
+                    </td>
+                    <td>
+                        <?php echo $enrollments[$i]["prod_price"] ?>
+                    </td>
+                </tr>
+<?php
+            }
+?>
+
+    </table>
+</body>
+</html>
